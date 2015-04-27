c7b841ec2a2c6fb551342aa87de3a3a54e80beb0
Márton Szinovszki <szinya@mentha.hu>
2015-04-27 16:39:31 +0200

    Update phpmyadmin4

---
+++ Formula/phpmyadmin4.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../../Abstract/abstract-phpmyadmin", __FILE__)
+
+class Phpmyadmin4 < AbstractPhpmyadmin
+  url 'https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_4_4_4.tar.gz'
+  sha256 '1fc8f6e9229f22920b06d27fda63ad39c906bd11e76d555d03f8878e9800627f'
+end
